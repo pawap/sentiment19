bb3e35a0d63e6e91476f7ad3449e65c1009d2b89
pawap <38111319+pawap@users.noreply.github.com>
2019-06-19 20:52:27 +0200

    amend dat sh...

--- src/main/java/sentiments/domain/repository/TrainingTweetRepository.java
+++ src/main/java/sentiments/domain/repository/TrainingTweetRepository.java
@@ -1,14 +1,15 @@
 package sentiments.domain.repository;
 
-import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import sentiments.domain.model.Tweet;
 
 public interface TrainingTweetRepository extends TweetRepository {
 	
-	@Query("from Tweet where test=:test and offensive=:offensive")
-	public Iterable<Tweet> findAllByTestAndOffensive(@Param("test") Boolean test, @Param("offensive") Boolean offensive);
+//	@Query("from Tweet where test=:test and offensive=:offensive")
+//	public Iterable<Tweet> findAllByTestAndOffensive(Boolean test, Boolean offensive);
 
-	@Query("select count(*) from Tweet where test=:test")
-	public int count(@Param("test") boolean test);
+	@Query(value="{}", count = true)
+	public int count(boolean test);
 }
