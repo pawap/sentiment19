09b9550e001be4bb6bc3691f2441eb9af4f2f100
6runge <28950411+6runge@users.noreply.github.com>
2019-06-23 15:25:41 +0200

    [TASK] Timestamp -> Date
    replaced sql timestamp with java.util date

--- src/main/java/sentiments/domain/model/AbstractTweet.java
+++ src/main/java/sentiments/domain/model/AbstractTweet.java
@@ -1,12 +1,7 @@
 package sentiments.domain.model;
 
-import java.sql.Timestamp;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.Date;
 
 @MappedSuperclass
 public class AbstractTweet {
@@ -16,8 +11,8 @@ public class AbstractTweet {
 	private Integer uid;
 	@Lob
 	private String text;
-	private Timestamp crdate;
-	private Timestamp tmstamp;
+	private Date crdate;
+	private Date tmstamp;
 	private boolean offensive;
 
 	public AbstractTweet() {
@@ -39,19 +34,19 @@ public class AbstractTweet {
 		this.text = text;
 	}
 
-	public Timestamp getCrdate() {
+	public Date getCrdate() {
 		return crdate;
 	}
 
-	public void setCrdate(Timestamp crdate) {
+	public void setCrdate(Date crdate) {
 		this.crdate = crdate;
 	}
 
-	public Timestamp getTmstamp() {
+	public Date getTmstamp() {
 		return tmstamp;
 	}
 
-	public void setTmstamp(Timestamp tmstamp) {
+	public void setTmstamp(Date tmstamp) {
 		this.tmstamp = tmstamp;
 	}
 
