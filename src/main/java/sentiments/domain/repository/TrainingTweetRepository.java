f859ee6d1cbcbbb887290d43aa9b8d076ff9481c
trew26 <spm@instant.de>
2019-07-03 11:59:10 +0200

    updated/added queries with comments

--- src/main/java/sentiments/domain/repository/TrainingTweetRepository.java
+++ src/main/java/sentiments/domain/repository/TrainingTweetRepository.java
@@ -8,12 +8,19 @@ import sentiments.domain.model.Tweet;
 
 public interface TrainingTweetRepository extends MongoRepository<Tweet,Integer> {
 
+	//return an Iterable wit all (non-) offensive tweets
 	@Query("{ 'offensive' : ?0 }")
 	public Iterable<Tweet> findAllByOffensive(Boolean offensive);
 
+	//returns an Iterable of all tweets that are labled as test/training and (non-) offensive
 	@Query("{ 'test' : ?1 , 'offensive' : ?0 }")
 	public Iterable<Tweet> findAllByTestAndOffensive(Boolean test, Boolean offensive);
 
+	//returns the count of all samples
 	@Query(value="{}", count = true)
 	public int count(boolean test);
+
+	//returns the count of test/training samples
+	@Query(value="{ 'test' : ?0 }", count = true)
+	public int countTest(boolean test);
 }
