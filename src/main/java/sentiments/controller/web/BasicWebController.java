d198dc3492e67e024c1409a70c105fa75a395734
6runge <28950411+6runge@users.noreply.github.com>
2019-09-18 22:29:02 +0200

    [TASK] controller structure refactoring
    + hashtag handling in frontend redone

---
+++ src/main/java/sentiments/controller/web/BasicWebController.java
@@ -0,0 +1,30 @@
+package sentiments.controller.web;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+public class BasicWebController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) throws Exception {
+        final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        final CustomDateEditor dateEditor = new CustomDateEditor(df, true) {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                if ("today".equals(text)) {
+                    setValue(new Date(System.currentTimeMillis()));
+                } else {
+                    super.setAsText(text);
+                }
+            }
+        };
+        binder.registerCustomEditor(Date.class, dateEditor);
+    }
+}
---
+++ src/main/java/sentiments/domain/service/ResponseService.java
@@ -0,0 +1,35 @@
+package sentiments.domain.service;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Paw, 6runge
+ */
+@Service
+public class ResponseService {
+
+    public String generateJSONResponse(String input) {
+        JSONObject out = new JSONObject();
+        out.put("input", input);
+
+        JSONArray sentiments = new JSONArray();
+
+        sentiments.add(input);
+
+        out.put("sentiments", sentiments);
+
+        return out.toString();
+    }
+
+    public String generateTextResponse(String input) {
+        StringBuilder output = new StringBuilder();
+
+        output.append("input: " + input);
+        output.append("\nsentiments:");
+        output.append(input);
+
+        return output.toString();
+    }
+}
