144f75173c29908738da48f18f7a0fc5924a42b1
pawap <38111319+pawap@users.noreply.github.com>
2019-09-29 11:16:34 +0200

    [TASK] Backend Auth with DB

--- src/main/java/sentiments/config/SecurityConfig.java
+++ src/main/java/sentiments/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package sentiments.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
@@ -22,12 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
+    public void configureGlobal(AuthenticationManagerBuilder auth, @Qualifier("backendUserService") UserDetailsService userDetailsService)
             throws Exception
     {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("{noop}ppp")
-                .roles("USER");
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
---
+++ src/main/java/sentiments/domain/service/BackendUserService.java
@@ -0,0 +1,26 @@
+package sentiments.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sentiments.domain.model.BackendUserPrincipal;
+import sentiments.domain.model.User;
+import sentiments.domain.repository.UserRepository;
+
+@Service
+public class BackendUserService  implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new BackendUserPrincipal(user);
+    }
+}
